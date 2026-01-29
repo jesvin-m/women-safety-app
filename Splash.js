9f0fd5e1bc31d32727a667de8a0264b0a60333c9
Pranav Arya R S <pranavarya2005@gmail.com>
2026-01-29 15:19:57 +0530

    Refactor Splash.js for better code organization
    Refactor Splash component to improve code structure and readability.

--- Splash.js
+++ Splash.js
@@ -1,37 +1,47 @@
-import { View, Text, StyleSheet, Image } from "react-native";
-import React from "react";
+import { View, StyleSheet, Image } from "react-native";
+import React, { useEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
-import { useEffect } from "react";
 import { auth } from "../../firebase";
 import { onAuthStateChanged } from "firebase/auth";
 
 const Splash = () => {
+  // Navigation hook to move between screens
   const navigation = useNavigation();
+
   useEffect(() => {
+    // Listen for authentication state changes
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
+        // If user is logged in, go to main app
         navigation.navigate("TabNavigator");
       } else {
+        // If no user, show splash for 3 seconds then go to Register
         setTimeout(() => {
           navigation.navigate("Register");
         }, 3000);
       }
     });
+
+    // Cleanup the listener when component unmounts
     return unsubscribe;
   }, []);
 
   return (
     <View style={styles.container}>
-      <Image style={styles.image} source={require("../../assets/safety.png")} />
+      {/* Splash screen image */}
+      <Image
+        style={styles.image}
+        source={require("../../assets/safety.png")}
+      />
     </View>
   );
 };
 
 export default Splash;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: "column",
     alignItems: "center",
     justifyContent: "center",
   },
